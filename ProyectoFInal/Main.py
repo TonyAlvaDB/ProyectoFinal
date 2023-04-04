cd3b4c2bf484ef318077a1e463b2660fc10a77a1
rogomez23 <123708414+rogomez23@users.noreply.github.com>
2023-04-04 12:03:48 -0600

    MainFileChange

--- ProyectoFInal/Main.py
+++ ProyectoFInal/Main.py
@@ -1,4 +1,11 @@
 from DataBase.DataBaseLogic import *
+import sys
+from  PyQt6 import QtCore,QtGui,QtWidgets
+from Application.Core.moduloCore import FrmMain
+
 
 if __name__ == "__main__":
-    
+    app = QtWidgets.QApplication(sys.argv)
+    window = FrmMain()
+    window.show()
+    sys.exit(app.exec())
