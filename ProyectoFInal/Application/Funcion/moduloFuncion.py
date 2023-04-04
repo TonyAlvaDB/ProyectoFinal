953d38567cc6dd3503eb17170cbe475d2b5be699
rogomez23 <123708414+rogomez23@users.noreply.github.com>
2023-04-04 12:02:57 -0600

    ConexiónPantallas

---
+++ ProyectoFInal/Application/Funcion/moduloFuncion.py
@@ -0,0 +1,42 @@
+import traceback
+from  PyQt6 import QtCore,QtGui,QtWidgets
+from UI.qtMainWindow import Ui_qtMainWindow
+from UI.qtWDWAgregarBodega import Ui_qtWDWAgregarBodega
+from UI.qtWDWControlDeBodegas import Ui_qtWDWControlDeBodegas
+from UI.qtWDWInventarios import Ui_qtWDWInventarios
+from UI.qtWDWRegistroDeArticulos import Ui_qtWDWRegistroDeArticulos
+from Application.Core.moduloCore import *
+
+class CreacionBodega(QtWidgets.QDialog):
+    def __init__(self) -> None:
+        super().__init__()
+        self.ui = Ui_qtWDWAgregarBodega() #La propieda ui contiene la instancia de la interfaz grafica que dibujamos
+        self.ui.setupUi(self) #Esta linea dibuja la interfaz grafica
+        
+
+
+class ModificarInventarios(QtWidgets.QDialog):
+    def __init__(self) -> None:
+        super().__init__()
+        self.ui = Ui_qtWDWInventarios()
+        self.ui.setupUi(self)
+        # self.ui.btnImprimir.clicked.connect(self.imprimirFacturas)
+        # self.ui.btnEliminarFactura.clicked.connect(self.eliminarFactura)
+
+
+class RegistroArticulos(QtWidgets.QDialog):
+    def __init__(self) -> None:
+        super().__init__()
+        self.ui = Ui_qtWDWRegistroDeArticulos()
+        self.ui.setupUi(self)
+        # self.ui.btnImprimir.clicked.connect(self.imprimirFacturas)
+        # self.ui.btnEliminarFactura.clicked.connect(self.eliminarFactura)
+
+
+class GestionBodegas(QtWidgets.QDialog):
+    def __init__(self) -> None:
+        super().__init__()
+        self.ui = Ui_qtWDWControlDeBodegas()
+        self.ui.setupUi(self)
+        # self.ui.btnImprimir.clicked.connect(self.imprimirFacturas)
+        # self.ui.btnEliminarFactura.clicked.connect(self.eliminarFactura)       
