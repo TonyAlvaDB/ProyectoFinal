0569be13547a9ed4651ed7966384f654a0b3c538
Anthony Alvarez Delgado <107961797+aalvarez-d@users.noreply.github.com>
2023-04-17 21:02:04 -0600

    base de datos

--- DataBase/DataBaseLogic.py
+++ DataBase/DataBaseLogic.py
@@ -1,67 +1,39 @@
-import mysql.connector
+import os
+import csv
 
+class FileManager:
+    def __init__(self, db_path):
+        self.db_path = db_path
 
-class BaseDeDatos:
-    def __init__(self, host, user, password, database) -> None:
-        self.host = host
-        self.user = user
-        self.password = password
-        self.database = database
-        self.conexion = None   
-        self.myCursor = None
-
-    
-    def conectarLaBaseDeDatos(self, host, user, password, database):
-        # Método para establecer la conexión con la base de datos
-        self.conexion = mysql.connector.connect(
-            host=host, 
-            user=user,
-            password=password,
-            database=database
-        )
-        if self.conexion.is_connected():
-            print("La conexión a la base de datos ha sido exitosa")
-            self.myCursor = self.conexion.cursor()
+    def create_table(self, table_name, columns):
+        table_file = os.path.join(self.db_path, f"{table_name}.csv")
+        if not os.path.exists(table_file):
+            with open(table_file, 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(columns)
+            print(f"La tabla '{table_name}' ha sido creada exitosamente.")
         else:
-            print("Error en la conexión")
-            
-    def desconectar(self):
-        # Método para cerrar la conexión con la base de datos
-        if self.conexion.is_connected():
-            self.myCursor.close()
-            self.conexion.close()
-            print("La conexión ha sido detenida")
+            print(f"La tabla '{table_name}' ya existe.")
+
+    def insert(self, table_name, data):
+        table_file = os.path.join(self.db_path, f"{table_name}.csv")
+        if os.path.exists(table_file):
+            with open(table_file, 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(data)
+            print("Datos insertados exitosamente.")
         else:
-            print("No existe conexión que cerrar")
+            print(f"La tabla '{table_name}' no existe.")
 
-    def crearBaseDeDatos(self, nombre):
-        # Método para crear una nueva base de datos
-        self.myCursor.execute(f'CREATE DATABASE {nombre}')
-        print("Base de datos creada correctamente")
+    def select_all(self, table_name):
+        table_file = os.path.join(self.db_path, f"{table_name}.csv")
+        if os.path.exists(table_file):
+            with open(table_file, 'r') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    print(row)
+        else:
+            print(f"La tabla '{table_name}' no existe.")
 
-    def crearTablaNueva(self, nombre):
-        # Método para crear una nueva tabla en la base de datos
-        self.myCursor.execute(f'CREATE TABLE {nombre} (id INT AUTO_INCREMENT PRIMARY KEY, Producto VARCHAR(255), Precio VARCHAR(255), Cantidad VARCHAR(255))')
-        print(f'Tabla {nombre} creada correctamente')
+    # Puedes implementar otras funcionalidades, como update, delete, etc., utilizando la misma lógica de manejo de archivos planos.
 
-    def ingresarDatos(self, nombre, precio, cantidad, tabla):
-        # Método para ingresar datos a una tabla en la base de datos
-        query = 'INSERT INTO {} (Producto, Precio, Cantidad) VALUES (%s, %s, %s)'.format(tabla)
-        values = (nombre, precio, cantidad)
-        self.myCursor.execute(query, values)
-        self.conexion.commit()
-        print(f"El producto {nombre} ha sido ingresado correctamente. Este se insertó en la fila {self.myCursor.lastrowid}")
-
-    def eliminarDatos(self, datoParaBuscar, auxiliarParaEliminar, tabla):
-        # Método para eliminar datos de una tabla en la base de datos
-        query = 'DELETE FROM {} WHERE {} = %s'.format(tabla, auxiliarParaEliminar)
-        values = (datoParaBuscar,)
-        self.myCursor.execute(query, values)
-        self.conexion.commit()
-        print("Datos eliminados correctamente")
-
-    def seleccionarTodosLosDatos(self, tabla) -> list:
-        # Método para seleccionar todos los datos de una tabla en la base de datos
-        self.myCursor.execute(f'SELECT * FROM {tabla}')
-        myresult = self.myCursor.fetchall()
-        return myresult
